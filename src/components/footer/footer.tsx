3e4305375c33f2ff67fecd2df503b69a2b09c713
Edimilson <valeedimilson@gmail.com>
2024-03-28 19:41:53 -0300

    adicionado pagina de cadastro e login(paginas PlaceHolder)

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <div>
+          2024 - SIREAG - Sistema de Reserva de Ambientes na Escola do Futuro de
+          Goiás
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import "./Styles/global.css";
 import { BrowserRouter } from "react-router-dom";
 import NavBar from "./components/navbar/navBar";
 import RotasPaginas from "./components/navbar/RotasPaginas";
+import Footer from "./components/footer/footer";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <NavBar />
-    <RotasPaginas />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <NavBar />
+      <RotasPaginas />
+    </BrowserRouter>
+    <Footer />
+  </>
 );
