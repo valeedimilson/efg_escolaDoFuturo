0c0415b260894babcd389e30600d56ccbafc350d
Dyme <valeedimilson@gmail.com>
2024-10-09 21:32:07 -0300

    Fix: Correção no navBar, e correção do estilo do Filter do componente reserva

--- src/components/navbar/navBar.tsx
+++ src/components/navbar/navBar.tsx
@@ -15,14 +15,28 @@ function NavBar() {
   const menuMobileBg = document.querySelector("#menuMobileBg");
   const [menuMobileAberto, setMenuMobileAberto] = useState(false);
 
-  function Botao() {
+  function toggleMenu(status?: boolean) {
+    console.log("foi clicado");
+    if (status == false) {
+      setMenuMobileAberto(false);
+    } else {
+      setMenuMobileAberto(!menuMobileAberto);
+    }
+  }
+
+  useEffect(() => {
+    menuMobileBg?.classList.toggle("menuMobileShow");
+    menuMobile?.classList.toggle("menuMobileShow");
+  }, [menuMobileAberto]);
+
+  function BotaoMenuMobile() {
     if (menuMobileAberto == false) {
       return (
         <IoEllipsisVerticalCircleOutline
           size="30"
           className="menuMobileBt"
           onClick={function () {
-            abreFechaMenu();
+            toggleMenu();
           }}
         />
       );
@@ -32,42 +46,19 @@ function NavBar() {
           size="30"
           className="menuMobileBt"
           onClick={function () {
-            abreFechaMenu();
+            toggleMenu();
           }}
         />
       );
     }
   }
 
-  function abreFechaMenu(estado?: boolean) {
-    if (estado == false) {
-      setMenuMobileAberto(false);
-    } else {
-      setMenuMobileAberto(!menuMobileAberto);
-    }
-  }
-
-  useEffect(() => {
-    if (menuMobileAberto) {
-      menuMobile?.classList.add("menuMobileShow");
-      menuMobileBg?.classList.add("menuMobileBgShow");
-
-      menuMobile?.classList.remove("menuMobileHidden");
-      menuMobileBg?.classList.remove("menuMobileBgHidden");
-    } else {
-      menuMobile?.classList.remove("menuMobileShow");
-      menuMobileBg?.classList.remove("menuMobileBgShow");
-
-      menuMobile?.classList.add("menuMobileHidden");
-      menuMobileBg?.classList.add("menuMobileBgHidden");
-    }
-  }, [menuMobileAberto]);
   return (
     <header>
       <Link
         to="/"
         onClick={function () {
-          abreFechaMenu(false);
+          toggleMenu(false);
         }}
       >
         <img
@@ -75,29 +66,122 @@ function NavBar() {
           alt="SIREAG - Sistema de Reserva de Ambientes na Escola do Futuro de Goiás"
         />
       </Link>
+
       <div className="menuDesktop">
         <Links />
       </div>
       <div
-        className="menuMobileBg"
+        className="menuMobileBg "
         id="menuMobileBg"
         onClick={function () {
-          abreFechaMenu(false);
+          toggleMenu();
         }}
       ></div>
-      <div className="menuMobile" id="menuMobile">
+      <div className="menuMobile " id="menuMobile">
         <div>
           <Links
             onClick={function () {
-              abreFechaMenu(false);
+              toggleMenu();
             }}
           />
         </div>
       </div>
       <div className="menuMobileBtdiv">
-        <Botao />
+        <BotaoMenuMobile />
       </div>
     </header>
   );
 }
+
 export default NavBar;
+// function NavBar() {
+//   const menuMobile = document.querySelector("#menuMobile");
+//   const menuMobileBg = document.querySelector("#menuMobileBg");
+//   const [menuMobileAberto, setMenuMobileAberto] = useState(false);
+
+// function Botao() {
+//   if (menuMobileAberto == false) {
+//     return (
+//       <IoEllipsisVerticalCircleOutline
+//         size="30"
+//         className="menuMobileBt"
+//         onClick={function () {
+//           abreFechaMenu();
+//         }}
+//       />
+//     );
+//   } else {
+//     return (
+//       <IoCloseCircleOutline
+//         size="30"
+//         className="menuMobileBt"
+//         onClick={function () {
+//           abreFechaMenu();
+//         }}
+//       />
+//     );
+//   }
+// }
+
+//   function abreFechaMenu(estado?: boolean) {
+//     if (estado == false) {
+//       setMenuMobileAberto(false);
+//     } else {
+//       setMenuMobileAberto(!menuMobileAberto);
+//     }
+//   }
+
+//   useEffect(() => {
+//     if (menuMobileAberto) {
+//       menuMobile?.classList.add("menuMobileShow");
+//       menuMobileBg?.classList.add("menuMobileBgShow");
+
+//       menuMobile?.classList.remove("menuMobileHidden");
+//       menuMobileBg?.classList.remove("menuMobileBgHidden");
+//     } else {
+//       menuMobile?.classList.remove("menuMobileShow");
+//       menuMobileBg?.classList.remove("menuMobileBgShow");
+
+//       menuMobile?.classList.add("menuMobileHidden");
+//       menuMobileBg?.classList.add("menuMobileBgHidden");
+//     }
+//   }, [menuMobileAberto]);
+//   return (
+//     <header>
+//       <Link
+//         to="/"
+//         onClick={function () {
+//           abreFechaMenu(false);
+//         }}
+//       >
+//         <img
+//           src={logo}
+//           alt="SIREAG - Sistema de Reserva de Ambientes na Escola do Futuro de Goiás"
+//         />
+//       </Link>
+// <div className="menuDesktop">
+//   <Links />
+// </div>
+// <div
+//   className="menuMobileBg"
+//   id="menuMobileBg"
+//   onClick={function () {
+//     abreFechaMenu(false);
+//   }}
+// ></div>
+// <div className="menuMobile" id="menuMobile">
+//   <div>
+//     <Links
+//       onClick={function () {
+//         abreFechaMenu(false);
+//       }}
+//     />
+//   </div>
+//       </div>
+// <div className="menuMobileBtdiv">
+//   <Botao />
+// </div>
+//     </header>
+//   );
+// }
+// export default NavBar;
